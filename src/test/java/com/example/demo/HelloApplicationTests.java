5127bd59ef41bcb5fa7babe39ef75b501628e3eb
JiChangYue <jcy18371314@163.com>
2022-06-22 14:36:42 +0800

    111

---
+++ src/test/java/com/example/demo/HelloApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ApplicationArguments.class)
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
